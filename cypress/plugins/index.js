829bd1bbc5f262e7e25019caeb23231c4e3920fa
bubykay <bubykay@gmail.com>
2021-12-08 07:13:45 +0100

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -16,7 +17,10 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-export default (on, config) => {
+// eslint-disable-next-line no-undef
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-undef
+module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
